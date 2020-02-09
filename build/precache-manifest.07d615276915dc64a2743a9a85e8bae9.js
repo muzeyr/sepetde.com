041355ab5dce284db4897e03d586ccb9722d59ea
muozcan <muzeyr@gmail.com>
2020-02-09 23:49:37 +0300

    colection item

---
+++ build/precache-manifest.07d615276915dc64a2743a9a85e8bae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f9a32868b9d2483f554b0e9e8840d9",
+    "url": "/sepetde.com/index.html"
+  },
+  {
+    "revision": "bf5ddbed75f3ac88dd64",
+    "url": "/sepetde.com/static/css/main.cd7e240b.chunk.css"
+  },
+  {
+    "revision": "19c47848b28a9825baaf",
+    "url": "/sepetde.com/static/js/2.e2fa374b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/sepetde.com/static/js/2.e2fa374b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5ddbed75f3ac88dd64",
+    "url": "/sepetde.com/static/js/main.9d9e59fa.chunk.js"
+  },
+  {
+    "revision": "cdcc14faf86e9e91c6fd",
+    "url": "/sepetde.com/static/js/runtime-main.edca54de.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/sepetde.com/static/media/crown.ed28accb.svg"
+  }
+]);
