e1e2780198c8fa5ca3daaa55790e480dfb4f204f
muozcan <muzeyr@gmail.com>
2020-02-09 12:23:12 +0300

    New version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './pages/homapage/homepage.component';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
